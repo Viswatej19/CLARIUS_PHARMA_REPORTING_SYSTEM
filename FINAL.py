7b029d7b6002028aef21b3266db10a553cf89d01
Viswatej Bhamidipati <vissu1521@gmail.com>
2026-05-06 11:47:26 +0530

    commited 30G

--- FINAL.py
+++ FINAL.py
@@ -655,7 +655,7 @@ for i in range(num_products):
         "Packing",
         options=[
             "1 x 10", "1 x 15" ,"2 x 10", "3 x 10", "4 x 10", "5 x 10",
-            "6 x 10", "7 x 10", "8 x 10", "9 x 10", "10 x 10"
+            "6 x 10", "7 x 10", "8 x 10", "9 x 10", "10 x 10", "1 x 30 GM"
         ],
         key=f"pk{i}"
     )
